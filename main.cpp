4cfdd469cb61266901ea580e333cbf9ab6147e8d
Nate Sire <natesire@gmail.com>
2025-11-23 16:05:39 -0500

    test first three integers

--- main.cpp
+++ main.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
+#include <cstdint>
+
+bool isOdd(long number) {
+	return number % 2 != 0;
+}
 
 int main () {
-	for(int i = 0; i < 10; i++) {
-		std::cout << "Iteration " << i << "\n";
-	}	return 0;
+	int squared_and_odd_summed = 0;
+	unsigned long squared = 0;
+	const int FIRST_Squared = 5;
+	int i = 0;
+	
+	while(true) {
+		i++;
+
+		if(i > FIRST_Squared) {
+			std::cout << "stopping at: " << i << "\n";
+			break;
+		}
+		
+		squared = i * i;
+
+		if(squared < 0) {
+			std::cout << "overflow detected at i: " << squared << "\n";
+			break;
+		}
+
+		if(isOdd(squared)) { 
+			std::cout << squared << " squared number is odd \n";
+			squared_and_odd_summed += squared; }
+		std::cout << "sum: " << squared_and_odd_summed << "\n";	
+	}	
+	
+	return squared_and_odd_summed;
 }
