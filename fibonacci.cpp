40364ecc5eb3d553acccd78e31fd2450a12c55b1
Nate Sire <natesire@gmail.com>
2025-11-25 05:20:38 -0500

    fibonacci code works.

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -3,12 +3,19 @@
 #include <vector>
 
 int main() {
-    int sum = 0;
+    int sum = 2;
     int new_term = 0;
     std::vector<int> fibonacci_terms = {1, 2};
-    for(int i = 0; i < fibonacci_terms.size(); ++i) {
-        new_term = fibonacci_terms[0] + fibonacci_terms[1];
-        if(new_term > 4000000) break;
+    for(int i = 0; i < 40000000; ++i) {
+        new_term = fibonacci_terms[i] + fibonacci_terms[i + 1];
+        fibonacci_terms.push_back(new_term);
+        if(new_term >= 4000000) {
+            break;
+        }
+        if(new_term % 2 == 0) {
+            sum += new_term;
+        }
     }
+    std::cout << "Sum of even Fibonacci terms below four million: " << sum;
     return 0;
 }
