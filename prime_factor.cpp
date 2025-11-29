b2b77ad82e5524d15d5e18d76892e9f956c9802c
Nate Sire <natesire@gmail.com>
2025-11-28 19:06:30 -0500

    raise n to power

--- prime_factor.cpp
+++ prime_factor.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <array>
 #include <vector>
+#include <cmath>
 
 struct Node {
     long value;
@@ -9,7 +10,7 @@ struct Node {
     std::vector<int> num;
 };
 
-long divBy(long val, long by) {
+long divBy(long val, int by) {
     if(val % by == 0) return val / by;
     return 0;
 }
@@ -22,34 +23,68 @@ bool isPrime(long val) {
     return true;
 }
 
+bool createNeighbors(Node* current) {
+    current->children.push_back(new Node{divBy(current->value, 2), {}});
+    current->children.push_back(new Node{divBy(current->value, 3), {}});
+    current->children.push_back(new Node{divBy(current->value, 5), {}});
+    current->children.push_back(new Node{divBy(current->value, 7), {}});
+    return true;
+}
+
+bool visited(std::vector<Node*> stack, Node* node) {
+    stack.push_back(node);
+    return true;
+}
+
+bool popStack(std::vector<Node*> stack) {
+    try {
+        if(stack.empty()) throw std::runtime_error("stack is empty, cannot pop");
+        stack.pop_back();
+    } catch(const std::runtime_error& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return false;
+    }
+    
+    return true;
+}
+
+bool pushStack(std::vector<Node*>& stack, Node* node) {
+    stack.push_back(node);
+    return true;
+}
+
 
 int main() {
     //long max_product = 600851475143;
-    //long max_product = 2 * 3 * 5 * 7;
-    long max_product = 2 * 2 * 2 * 2 * 2;
-    Node* root = new Node{max_product, {}};
+    long product = pow(2, 10);
 
     std::vector<Node*> nodeStack; 
 
-    std::cout << "is prime " << max_product << ": " << isPrime(max_product) << std::endl;
+    std::cout << "is prime " << product << ": " << isPrime(product) << std::endl;
     
-    try {
-        root->children.push_back(new Node{divBy(root->value, 2), {}});
-        root->children.push_back(new Node{divBy(root->value, 3), {}});
-        root->children.push_back(new Node{divBy(root->value, 5), {}});
-        root->children.push_back(new Node{divBy(root->value, 7), {}});
+    // create depth-first through tree with stack
+    Node* root = new Node{product, {}};
+    int v;
+    Node* current = nullptr;
+    pushStack(nodeStack, root);
+    while(nodeStack.size() > 0) { // nodes unvisited
+        current = nodeStack.back();
+        createNeighbors(current);
+        visited(nodeStack, current);
 
-    //nodeStack.push_back(root->next2);
-    } catch(const std::exception& e) {
-        std::cout << "exception caught " << e.what() << std::endl;
-    }
-    
-    // iterate depth-first through tree
-    Node* current = root;
-    while(current != nullptr) { // nodes unvisited
-        nodeStack.push_back(current);
-        int childIndex = 0;
-        current = current->children.empty() ? nullptr : current->children[0];
+        v = divBy(current->value, 2); // 0 should terminate this branch
+        std::cout << "divBy result: " << v << std::endl;
+        current->children[0]->value = v;
+        if(v == 1) popStack(nodeStack);
+        if(v > 0) pushStack(nodeStack, current->children[0]);
+        if(nodeStack.empty()) {
+            std::cout << "stack empty, terminating" << std::endl;
+            break;
+        }
+        current = nodeStack.back();
+
+        //int childIndex = 0;
+        //current = current->children.empty() ? nullptr : current->children[0];
         //current->children.push_back(new Node{divBy(current->value, 2), {}});
         if(current != nullptr) { 
             std::cout << "current value:" << current->value << std::endl;
